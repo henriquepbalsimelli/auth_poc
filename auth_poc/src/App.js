9cf3e92e851622c7980362ca86ad9ecd5da7f766
henrique-paes <henrique.paes@shopper.com.br>
2023-02-08 18:26:21 -0300

    feature: login basico com google identity services

--- auth_poc/src/App.js
+++ auth_poc/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import AppContext from './AppContext';
 import PainelPage from './pages/Painel/PainelPage';
 import PedidoAvulso from './pages/PedidoAvulso/PedidoAvulso';
 import {
@@ -8,20 +10,24 @@ import {
   Route,
 } from "react-router-dom";
 import CriarPedido from './pages/CriarPedido/CriarPedido';
-
+import Authentication from './pages/Authentication/Authentication';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/painel" element={<PainelPage />} />
-        <Route path="/pedidos/avulso" element={<PedidoAvulso />} />
-        <Route path="/criarPedido" element={<CriarPedido />} />
-        
-      </Routes>
-    </BrowserRouter>
 
+  const [user, setUser] = useState()
+
+  return (
+    <AppContext.Provider value={{user, setUser}}>
+      <Authentication />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/painel" element={<PainelPage />} />
+          <Route path="/pedidos/avulso" element={<PedidoAvulso />} />
+          <Route path="/criarPedido" element={<CriarPedido />} />
+        </Routes>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 }
 
-export default App;
+export default App
---
+++ auth_poc/src/pages/Authentication/Authentication.jsx
@@ -0,0 +1,104 @@
+import { createRef, useContext, useEffect } from "react"
+import AppContext from "../../AppContext"
+import jwtDecode from 'jwt-decode'
+
+function Authentication() {
+
+    const loginButton = createRef()
+    const { user, setUser } = useContext(AppContext)
+
+    useEffect(() => {
+        
+        loadUserSession()
+        inicializarLogin()
+
+    }, [user])
+    
+    function loadUserSession() {
+        const token = sessionStorage.getItem('user')
+        if (token) {
+            setGoogleUser(token)
+        }
+    }
+
+    function inicializarLogin() {
+
+        if (user) {
+            return
+        }
+
+        /* global google */
+        google.accounts.id.initialize({
+            client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+            callback: handleLoginCallback
+        })
+
+        if (!user) {          
+            google.accounts.id.renderButton(loginButton.current, {
+                theme: 'filled_blue',
+                size: 'large'
+            })
+        }
+    }
+
+    function handleLoginCallback(response) {
+        if (response.credential) {
+            setGoogleUser(response.credential)
+            sessionStorage.setItem('user', response.credential)
+        }
+    }
+
+    function setGoogleUser(token) {
+        const goooglePayload = jwtDecode(token)
+
+        const googleUser = {
+            email: goooglePayload.email,
+            email_verified: goooglePayload.email_verified,
+            name: goooglePayload.name,
+            picture: goooglePayload.picture  
+        }
+
+        setUser(googleUser)
+    }
+
+    function handleLogout() {
+        setUser(null)
+        sessionStorage.removeItem('user')
+        window.location.href = '/'
+    }
+
+    return(
+        <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+            { !user && 
+                <div ref={loginButton} id="loginButton"></div>
+            }            
+
+            { user && 
+                <footer style={{ 
+                    position: "fixed", 
+                    bottom: 0, 
+                    minHeight: "60px",
+                    width: "100%",
+                    background: "#444",
+                    color: "#eee",
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                     }}>
+
+                    <div style={{display: "flex" }}>
+                        <p><strong>Usuário:</strong> <span>{user.name}</span></p>
+                        <p><strong>Email:</strong> <span>{user.email}</span></p>
+                    </div>
+
+                    <div>
+                        <a style={{color: "#eee", margin: "0 2rem", fontWeight: "bold"}} onClick={handleLogout}>Trocar Usuário</a>
+                    </div>
+                    
+                </footer>
+            }
+        </div>
+    )
+}
+
+export default Authentication
